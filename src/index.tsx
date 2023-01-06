101cf4d74530a13a87cecd5eaa0f37a84e2d1bb3
Joe Ng <joengsh@gmail.com>
2023-01-06 17:59:51 +0000

    build: remove export of missing component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
 export { default as Carousel } from '@components/molecules/Carousel/Carousel';
 export { default as Hero } from '@components/organisms/Hero/Hero';
 export { default as Footer } from '@components/organisms/Footer/Footer';
-export { default as Navbar } from '@components/organisms/Navbar/Navbar';
